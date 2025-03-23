ad768f795c346e2edd57e5a7dd9c801895d6207e
Nikita-Fast <breakdownofsanity1404@gmail.com>
2025-03-23 20:37:29 +0300

    пример про хаббл

--- Source/spatial_filtering.py
+++ Source/spatial_filtering.py
@@ -50,15 +50,15 @@ def get_weigths(w_size):
 
 if __name__ == '__main__':
 
-    p = r'D:\Projects\pythonProject\pythonProject\ML2025\Resources\DIP3E_Original_Images_CH03\Fig0333(a)(test_pattern_blurring_orig).tif'
-    img = pic_to_array(p)
-
+    # p = r'D:\Projects\pythonProject\pythonProject\ML2025\Resources\DIP3E_Original_Images_CH03\Fig0333(a)(test_pattern_blurring_orig).tif'
+    #
     # for w_size in [3, 5, 9, 15, 25]:
     #     print(f"Однородное среднее {w_size}x{w_size} ...")
     #
+    #     img = pic_to_array(p)
     #     res = filter2d(img, np.mean, w_size)
     #     save_img(res, f"../Results/homo_blur_{w_size}x{w_size}")
-
+    #
     # for w_size in [3, 5, 9, 15, 25]:
     #     print(f"Взвешенное среднее {w_size}x{w_size} ...")
     #
@@ -66,8 +66,39 @@ if __name__ == '__main__':
     #     def weighted_average(x):
     #         return np.sum(x * w) / np.sum(w)
     #
+    #     img = pic_to_array(p)
     #     res = filter2d(img, weighted_average, w_size)
     #     save_img(res, f"../Results/weighted_blur_{w_size}x{w_size}")
+
+    p = r'D:\Projects\pythonProject\pythonProject\ML2025\Resources\DIP3E_Original_Images_CH03\Fig0334(a)(hubble-original).tif'
+    img = pic_to_array(p)
+
+    res = filter2d(img, np.mean, w_size=15)
+    counts, bins = np.histogram(res, range(257))
+
+    plt.plot(np.cumsum(counts) / np.prod(img.shape) * 100)
+    plt.title("Доля пикселей с яркостью не превышающей порог")
+    plt.ylabel("Доля пикселей, % ")
+    plt.xlabel("Порог")
+    # plt.axvline(56, 0, 100, c='r')
+    # plt.xlim(0, 100)
+    # plt.ylim(50, 100)
+    plt.show()
+
+    def get_threshold(remove_pixel_percent):
+        i_bin = np.where((np.cumsum(counts) / np.prod(img.shape)) * 100 > remove_pixel_percent)[0][0]
+        thresh = bins[i_bin]
+        print(f"Порог для отсеивания {remove_pixel_percent}% пикселей равен {thresh}")
+        return thresh
+
+    for _threshold in [get_threshold(95)]:
+        print(f"Пример про снимок с телескопа Хаббл, _threshold={_threshold} ...")
+
+        def smoothing_and_thresholding(window):
+            return 255 if np.mean(window) > _threshold else 0
+
+        res = filter2d(img, smoothing_and_thresholding, w_size=15)
+        save_img(res, f"../Results/hubble_treshold_{_threshold}")
 
     # path = r'D:\Projects\pythonProject\pythonProject\ML2025\Resources\DIP3E_Original_Images_CH03\Fig0335(a)(ckt_board_saltpep_prob_pt05).tif'
     # img = pic_to_array(path)
